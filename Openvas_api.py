d25ca3464902a72291af10cf1eace1a967577d30
mehdi <emahdij@gmail.com>
2019-04-29 21:25:06 +0430

    bugs: xml report credential done and add remove function

--- Openvas_api.py
+++ Openvas_api.py
@@ -1,13 +1,14 @@
-#requirement
-#gvm-tools
+# requirement
+# gvm-tools
 import gvm
 import xmltodict
 from gvm.connections import UnixSocketConnection
 from gvm.protocols.latest import Gmp
 import datetime
 import json
-scan_config_id= {
-    #for more information click here: https://docs.greenbone.net/GSM-Manual/gos-3.1/en/scanning.html
+
+scan_config_id = {
+    # for more information click here: https://docs.greenbone.net/GSM-Manual/gos-3.1/en/scanning.html
     'Discovery': '8715c877-47a0-438d-98a3-27c7a6ab2196',
     'Host Discovery': '2d3f051c-55ba-11e3-bf43-406186ea4fc5',
     'System Discovery': 'bbca7412-a950-11e3-9109-406186ea4fc5',
@@ -18,25 +19,28 @@ scan_config_id= {
     'scanner_id': '08b69003-5fc2-4037-a479-93b440211c73',
 }
 
-report_format_id= {
+report_format_id = {
     'html': '6c248850-1f62-11e1-b082-406186ea4fc5',
     'pdf': 'c402cc3e-b531-11e1-9163-406186ea4fc5',
     'xml': '5057e5cc-b825-11e4-9d0e-28d24461215b',
 }
 
+
 class gvm_api:
-    def __init__(self,gvmuser,gvmpass,host='127.0.0.1',):
-        self._gvmuser=gvmuser
-        self._gvmpass=gvmpass
-        self._host=host
+    def __init__(self, gvmuser, gvmpass, host='127.0.0.1', ):
+        self._gvmuser = gvmuser
+        self._gvmpass = gvmpass
+        self._host = host
 
     def connect(self):
-        connection=gvm.connections.TLSConnection(hostname=self._host)
-        self._gmp = Gmp(connection=connection)
-        self._gmp.authenticate(username=self._gvmuser,password=self._gvmpass)
-        self._gmp.connect()
 
-    def report(self,report_id,report_format):
+        connection = gvm.connections.TLSConnection(hostname=self._host)
+        self._gmp = Gmp(connection=connection)
+        self._gmp.authenticate(username=self._gvmuser, password=self._gvmpass)
+        self._gmp.connect()
+        return 'connect successfully';
+
+    def report(self, report_id, report_format):
         response = self._gmp.get_report(
             report_id=report_id, report_format_id=report_format_id.get(report_format)
         )
@@ -44,26 +48,52 @@ class gvm_api:
         print(response)
         return dict_xml['get_reports_response']['report']['#text']
 
-    def newscan(self,target,scanconfig):
-        # for more information about scan config click here: https://docs.greenbone.net/GSM-Manual/gos-3.1/en/scanning.html
-        target_id = self._create_target(target)
-        task_id=self._create_task(target,target_id,scanconfig,scan_config_id.get('scanner_id'))
-        report_id = self._start_task(task_id)
-        return report_id
-
     def get_scan_config(self):
         response = self._gmp.get_configs()
-        jsonconf=json.loads(json.dumps(xmltodict.parse(response)))
+        jsonconf = json.loads(json.dumps(xmltodict.parse(response)))
         out = []
-        for i in range(len(jsonconf['get_configs_response']['config'])-1):
-                out.append(json.dumps({
-                "name":jsonconf['get_configs_response']['config'][i]['name'],
-                "comment":jsonconf['get_configs_response']['config'][i]['comment'],
-                "id":jsonconf['get_configs_response']['config'][i]['@id']
+        for i in range(len(jsonconf['get_configs_response']['config']) - 1):
+            out.append(json.dumps({
+                "name": jsonconf['get_configs_response']['config'][i]['name'],
+                "comment": jsonconf['get_configs_response']['config'][i]['comment'],
+                "id": jsonconf['get_configs_response']['config'][i]['@id']
             }))
         return out
 
-    def _create_target(self,target):
+    def newscan(self, target, scanconfig):
+        # for more information about scan config click here: https://docs.greenbone.net/GSM-Manual/gos-3.1/en/scanning.html
+        target_id = self._create_target(target)
+        task_id = self._create_task(target, target_id, scanconfig, scan_config_id.get('scanner_id'))
+        report_id = self._start_task(task_id)
+        return report_id
+
+    def removeData(self):
+        tasks = self._gmp.get_tasks(
+            filter="rows=-1 not status=Running and "
+                   "not status=Requested and not "
+                   "status=&quot;Stop Requested&quot;")
+        tasks_dict = xmltodict.parse(tasks)
+        if tasks_dict['get_tasks_response']['task_count']['#text'] != '1':
+            for taid in range(int(tasks_dict['get_tasks_response']['task_count']['#text'])):
+                print(self._gmp.delete_task(tasks_dict['get_tasks_response']['task'][taid]['@id']))
+        else:print(self._gmp.delete_task(tasks_dict['get_tasks_response']['task']['@id']))
+        targets = self._gmp.get_targets(filter="rows=-1 not _owner=&quot;&quot;")
+        target_dict = xmltodict.parse(targets)
+        if  target_dict['get_targets_response']['target_count']['#text'] != '1':
+            for taid in range(int(target_dict['get_targets_response']['target_count']['#text'])):
+                print(self._gmp.delete_target(target_dict['get_targets_response']['target'][taid]['@id']))
+        else: print(self._gmp.delete_target(target_dict['get_targets_response']['target']['@id']))
+
+        credentials = self._gmp.get_credentials(filter="rows=-1 not _owner=&quot;&quot;")
+        credentials_dict = xmltodict.parse(credentials)
+        if credentials_dict['get_credentials_response']['credential_count']['#text'] != '1':
+            for cid in range (int(credentials_dict['get_credentials_response']['credential_count']['#text'])):
+                print(self._gmp.delete_credential(credentials_dict['get_credentials_response']['credential'][cid]['@id']))
+        else:print(self._gmp.delete_credential(credentials_dict['get_credentials_response']['credential']['@id']))
+        status_text = self._gmp.empty_trashcan()
+        return 'Done'
+
+    def _create_target(self, target):
         # create a unique name by adding the current datetime
         name = "Suspect Host {} {}".format(target, str(datetime.datetime.now()))
         response = self._gmp.create_target(name=name, hosts=[target])
@@ -79,7 +109,7 @@ class gvm_api:
             scanner_id=scanner_id,
         )
         task_id_xml = xmltodict.parse(response)
-        task_id=task_id_xml['create_task_response']['@id']
+        task_id = task_id_xml['create_task_response']['@id']
         print(task_id)
         return task_id
 
